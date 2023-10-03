05db3d6dc1d9eafc3f37b2a94752e78f218c4421
Charlie Ward <59178341+Charlie-Ward@users.noreply.github.com>
2023-10-03 09:36:21 +0100

    Initial Commit

---
+++ main.py
@@ -0,0 +1,18 @@
+
+def month_amount(month, year):
+    match month:
+        case "Jan" | "Mar" | "May" | "Jul" | "Aug" | "Oct" | "Dec":
+            print("31")
+        case "Apr" | "Jun" | "Sep" | "Nov":
+            print("30")
+        case "Feb":
+            if year % 4 == 0:
+                print("29")
+            else:
+                print("28")
+
+print("Month is the first three letter eg: Jan")
+month = input("Please input a month: ")
+year = int(input("Please input a year: "))
+
+month_amount(month, year)
